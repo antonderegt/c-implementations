eec1e10774a70dee57d344f897496f53a18cccec
Anton <antonderegt@pm.me>
2020-04-11 17:22:00 +0200

    Adds assert tests

--- queues/main.c
+++ queues/main.c
@@ -1,19 +1,18 @@
 #include <stdio.h>
 #include "queue.h"
+#include <assert.h>
 
 int main() {
     queue *q = initQueue();
+    assert(empty(q) == true);
 
     enqueue(q, 0);
     enqueue(q, 1);
     enqueue(q, 2);
+    assert(empty(q) == false);
 
-    // printf("q->tail: %d\n", q->tail->data);
-    // printf("q->head: %d\n", q->head->data);
-
-    printf("%d\n", dequeue(q));
-    // printf("q->head: %d\n", q->head->data);
-    printf("%d\n", dequeue(q));
+    assert(dequeue(q) == 0);
+    assert(dequeue(q) == 1);
 
     return 0;
 }
