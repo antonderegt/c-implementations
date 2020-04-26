a452288b8446ca17cdb90c28d4038906042840c8
Anton de Regt <antonderegt@pm.me>
2020-04-26 10:23:13 +0200

    Adds print_graph

--- graphs/graph.h
+++ graphs/graph.h
@@ -1,13 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-typedef struct {
-    int vertices;
-    int edges;
-    
-    struct vertex {
-        int size;
-        int cap;
-        int list[];
-    } * adj_list[];
-    
-} graph;
+struct edge_list {
+    int vertex;
+    struct edge_list* next;
+};
+
+typedef struct edge_list edge_list;
+
+void print_graph(edge_list** graph, int size);
+edge_list* init_graph(int size);
+void add_edge(edge_list** graph, int v1, int v2);
--- graphs/main.c
+++ graphs/main.c
@@ -1,6 +1,13 @@
 #include "graph.h"
 
 int main() {
+    int size = 10;
+    edge_list* graph = init_graph(size);
+
+    add_edge(&graph, 0, 1);
+    add_edge(&graph, 1, 2);
+
+    print_graph(&graph, 10);
 
     return 0;
 }
